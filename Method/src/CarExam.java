bc37b48d5d43b7eaa239a2e93734c1cd1f1aefa5
KimTimo <eogus8875@naver.com>
2021-05-03 14:53:30 +0900

    Method 프로젝트 생성

---
+++ Method/src/CarExam.java
@@ -0,0 +1,24 @@
+
+
+public class CarExam {
+
+	public static void main(String[] args) {
+
+		Car c1 = new Car();
+		Car c2 = new Car();
+		
+		c1.name = "¸ÓÇö";
+		c1.number = 8548;
+		
+		c2.name = "°ïµà";
+		c2.number = 3834;
+		
+		System.out.println(c1.name);
+		System.out.println(c1.number);
+		
+		System.out.println(c2.name);
+		System.out.println(c2.number);
+		
+	}
+
+}
---
+++ Method/src/StringMethodExam.java
@@ -0,0 +1,9 @@
+
+public class StringMethodExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
