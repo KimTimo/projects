f3d922c32c98a9751a26176bea86b3dea6594d4e
KimTimo <eogus8875@naver.com>
2021-05-04 04:49:44 +0900

    StringMethodExam 추가

--- Method/src/StringMethodExam.java
+++ Method/src/StringMethodExam.java
@@ -2,8 +2,17 @@
 public class StringMethodExam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//String srt = new String("hello");
+		String str = "hello";
+
+		System.out.println(str.length());
+		System.out.println(str.concat(" world"));
+		str = str.concat(" world");
+		System.out.println(str);
+		
+		System.out.println(str.substring(3));
+		System.out.println(str.substring(3, 7));
 	}
 
 }
