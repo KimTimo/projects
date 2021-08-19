aab9e69cf83c825bbb0e800f13d373f9091e802e
KimTimo <eogus8875@naver.com>
2021-08-19 18:06:39 +0900

    MybatisOracleGuestBook Upload

---
+++ MybatisOracleGuestBook/src/main/java/kr/green/guestbook/vo/TestVO.java
@@ -0,0 +1,12 @@
+package kr.green.guestbook.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class TestVO {
+	private Date today;
+	private int  result;
+	private String message;
+}
