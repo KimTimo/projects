b9a5fc558cf5ae126b70a52077d11c2e0eaa7bd8
KimTimo <eogus8875@naver.com>
2021-08-24 18:20:56 +0900

    Category2 Upload

---
+++ CategoryEx2/src/main/java/kr/green/category/vo/TestVO.java
@@ -0,0 +1,12 @@
+package kr.green.category.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class TestVO {
+	private Date today;
+	private int  result;
+	private String message;
+}
