a18c9ebe01f337a1f32388385c02d2864719a8d9
KimTimo <eogus8875@naver.com>
2021-09-10 17:16:02 +0900

    MVC Project Upload

---
+++ MVC/SpringMVCEx09_ibatisMariaDBPeople/src/main/java/kr/green/mvc08/PeopleController.java
@@ -0,0 +1,56 @@
+package kr.green.mvc08;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.green.mvc08.service.PeopleService;
+import kr.green.mvc08.vo.PeopleVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class PeopleController {
+	
+	@Autowired
+	private PeopleService peopleService;
+	
+	// 리스트
+	@RequestMapping(value = "/list") // 메서드와 주소를 연결하고 뷰페이지를 리턴하도록 만든다.
+	public String getAll(Model model) {
+		log.debug(this.getClass().getName() + "의 getAll 호출");
+		List<PeopleVO> list = peopleService.selectList();
+		log.debug(this.getClass().getName() + "의 getAll 결과 : " + list);
+		model.addAttribute("list", list);
+		return "list";
+	}
+	// 저장
+	@RequestMapping(value = "/insertOk") // 메서드와 주소를 연결하고 뷰페이지를 리턴하도록 만든다.
+	public String insert(@ModelAttribute PeopleVO peopleVO) { // @ModelAttribute : VO로 데이터 받기
+		log.debug(this.getClass().getName() + "의 insert 호출 : " + peopleVO);
+		peopleService.insert(peopleVO);		
+		return "redirect:/list";
+	}
+	
+	
+	// 수정
+	@RequestMapping(value = "/updateOk") // 메서드와 주소를 연결하고 뷰페이지를 리턴하도록 만든다.
+	public String update(@ModelAttribute PeopleVO peopleVO) { // @ModelAttribute : VO로 데이터 받기
+		log.debug(this.getClass().getName() + "의 update 호출 : " + peopleVO);
+		peopleService.update(peopleVO);
+		return "redirect:/list";
+	}
+	
+	// 삭제
+	@RequestMapping(value = "/deleteOk") // 메서드와 주소를 연결하고 뷰페이지를 리턴하도록 만든다.
+	public String delete(@ModelAttribute PeopleVO peopleVO) { // @ModelAttribute : VO로 데이터 받기
+		log.debug(this.getClass().getName() + "의 delete 호출 : " + peopleVO);
+		peopleService.delete(peopleVO.getId());
+		return "redirect:/list";
+	}
+	
+}
