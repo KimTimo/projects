a4efc22f62756f21e40526bb26793e60cb7c2130
KimTimo <eogus8875@naver.com>
2021-12-20 15:44:33 +0900

    어노테이션 기반으로 코드 개선 완료

--- src/main/java/com/timo/book/springboot/web/IndexController.java
+++ src/main/java/com/timo/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.timo.book.springboot.web;
 
+import com.timo.book.springboot.config.auth.LoginUser;
 import com.timo.book.springboot.config.auth.dto.SessionUser;
 import com.timo.book.springboot.service.PostsService;
 import com.timo.book.springboot.web.dto.PostsResponseDto;
@@ -16,12 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null){
             model.addAttribute("userName", user.getName());
         }
