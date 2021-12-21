ff31c6e79ccd6b70c5cf9556a6cd9282c69439ae
KimTimo <eogus8875@naver.com>
2021-12-21 15:56:17 +0900

    스프링 시큐리티 적용 및 테스트 코드 문제 해결 완료

--- src/main/java/com/timo/book/springboot/Application.java
+++ src/main/java/com/timo/book/springboot/Application.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+// @EnableJpaAuditing가 삭제
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class, args);
     }
 }
