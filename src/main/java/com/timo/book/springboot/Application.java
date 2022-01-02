4e40fc43e1a9b6836428f7e74ffc3d861051afa7
KimTimo <eogus8875@naver.com>
2022-01-02 16:35:32 +0900

    주석수정

--- src/main/java/com/timo/book/springboot/Application.java
+++ src/main/java/com/timo/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// @EnableJpaAuditing가 삭제
+// @EnableJpaAuditing를 삭제
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
