3ef000608a9275085853125db08fef76c4216f59
KimTimo <eogus8875@naver.com>
2022-01-07 16:32:00 +0900

    oembed를 활용한 웹서비스 구현

---
+++ spring-oembed-webservice/src/test/java/com/timo/book/SpringOembedWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timo.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOembedWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
